0af2cfd2d10f67f7b46e5b93748f3efebed8e414
Nata19754 <chavesnatally0419@gmail.com>
2023-02-02 11:07:18 -0800

    made requested changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 
   return(
   <>
-  <h4> Created by Natally,Chistian and Bora | Hotel 2023 </h4>
+    <h4> &copy; Created by Natally, Chistian and Bora || Hotel 2023 </h4>
   </>
   )
 }
